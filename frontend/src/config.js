ca674a00b6c01e6e0fbb3311c8fbafb1ebf9c04a
S-HariNarayan <harinarayan.s2024cce@sece.ac.in>
2026-06-07 21:19:15 +0530

    Delete frontend directory

--- frontend/src/config.js
+++
@@ -1,3 +0,0 @@
-const hostname = window.location.hostname === 'localhost' ? '127.0.0.1' : window.location.hostname;
-export const API_BASE_URL = `http://${hostname}:5000/api`;
-export const BASE_URL = `http://${hostname}:5000`;
